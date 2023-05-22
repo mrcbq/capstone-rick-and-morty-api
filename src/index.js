87165674a5a684487662a0aee1d5bdfc58b1226e
mrcbq <marcobaquero@gmail.com>
2023-05-22 18:45:21 -0500

    Fix error in popupComments, Fix linters errors
    Co-authored-by: Luiscarlosvd <luis.carlos.vad@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import './index.css';
-import popupComments from './modules/commentsPopup';
+import popupComments from './modules/commentsPopup.js';
 
 const buttonComments = document.querySelectorAll('.button-comments');
 const containerPopup = document.querySelector('.popup');
 
 const fetchPopupDetails = async (id) => {
-    try {
-      const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-      const data = await response.json();
-      console.log(data);
-      popupComments(data, containerPopup);
-    } catch {
-      alert('Error trying to connect with the API');
-    }
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/${id}`,
+    );
+    const data = await response.json();
+    console.log(data);
+    popupComments(data, containerPopup);
+  } catch {
+    alert('Error trying to connect with the API');
+  }
 };
 
-for(let i = 0; i < buttonComments.length; i++){
+for (let i = 0; i < buttonComments.length; i += 1) {
   buttonComments[i].addEventListener('click', () => {
-    fetchPopupDetails(i + 1);
-  })
+    fetchPopupDetails(i + 1); // es mejor traer todos los id con 1 llamada a la API...
+  });
 }
--- src/modules/commentsPopup.js
+++ src/modules/commentsPopup.js
@@ -1,6 +1,5 @@
-
 const popupComments = (data, container) => {
-    container.innerHtml = `
+  container.innerHTML = `
     <div class="popup-ele">
         <h1>Holaa ${data.name}</h1>
     </div>

3cd6d27a4f64cbcd9d5829ac95419054cdcfff55
Luiscarlosvd <lc26cr7@gmail.com>
2023-05-23 12:36:40 -0400

    Update popup window layout, description elements and fix Event listener for buttons

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import './index.css';
-import popupComments from './modules/commentsPopup.js';
+import "./index.css";
+import fetchPopup from "./modules/commentsPopup.js";
 
-const buttonComments = document.querySelectorAll('.button-comments');
-const containerPopup = document.querySelector('.popup');
+const buttonComments = document.querySelectorAll(".button-comments");
+const containerPopup = document.querySelector(".popup");
+const container = document.querySelector('.container-cards');
 
-const fetchPopupDetails = async (id) => {
-  try {
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/character/${id}`,
-    );
-    const data = await response.json();
-    console.log(data);
-    popupComments(data, containerPopup);
-  } catch {
-    alert('Error trying to connect with the API');
-  }
-};
 
-for (let i = 0; i < buttonComments.length; i += 1) {
-  buttonComments[i].addEventListener('click', () => {
-    fetchPopupDetails(i + 1); // es mejor traer todos los id con 1 llamada a la API...
-  });
-}
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('button-comments')) {
+        const id = e.target.parentElement.id;
+        fetchPopup(id, containerPopup);
+    }
+});
--- src/modules/commentsPopup.js
+++ src/modules/commentsPopup.js
@@ -1,9 +1,41 @@
+import quitIcon from '../img/icons8-x-50 (1).png';
+
+const fetchPopupDetails = async (id, container) => {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${id}`,
+      );
+      const data = await response.json();
+      console.log(data);
+      popupComments(data, container);
+    } catch {
+      alert('Error trying to connect with the API');
+    }
+};
+
 const popupComments = (data, container) => {
   container.innerHTML = `
     <div class="popup-ele">
-        <h1>Holaa ${data.name}</h1>
+        <div class="button-div">
+            <button alt="quit button" id="quit" class="quit-button"><img src="${quitIcon}"></button>
+        </div>
+        <div class="popup-description">
+            <img src="${data.image}">
+            <h1>${data.name}</h1>
+            <div class="popup-description-elements">
+                <p>Status: ${data.status}</p>
+                <p>Species: ${data.species}</p>
+                <p>Gender: ${data.gender}</p>
+                <p>Origin: ${data.origin.name}</p>
+                <p>Location: ${data.location.name}</p>
+            </div>
+        </div>
     </div>
     `;
+    const quitButton = document.querySelector('#quit');
+    quitButton.addEventListener('click', () => {
+        container.innerHTML = '';
+    })
 };
 
-export default popupComments;
+export default fetchPopupDetails;

1c8609fde3a61552087badc70615716d02738714
mrcbq <marcobaquero@gmail.com>
2023-05-22 23:00:14 -0500

    Add card genrator, random array generator, fetch rick and morty with random array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './index.css';
+import charactersArray from './modules/characters.js';
+import createCard from './modules/createCard.js';
+
+const container = document.getElementById('card-container');
+
+console.log(charactersArray);
+
+charactersArray.forEach((character) => {
+  const card = createCard(character);
+  container.appendChild(card);
+});

fca84e6fe56caa5d35f6d300b02edb0161846881
Luiscarlosvd <lc26cr7@gmail.com>
2023-05-24 15:31:01 -0400

    Create counterComments function that changes the text content of comments heading

---
+++ src/modules/counterComments.js
@@ -0,0 +1,10 @@
+const counterComments = (commentsContainer) => {
+    let counter = 0
+
+    if(commentsContainer.firstChild.nextElementSibling){
+      counter = commentsContainer.childElementCount;
+    }
+    return counter;
+};
+
+export default counterComments;

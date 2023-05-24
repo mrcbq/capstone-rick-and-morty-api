11a102b75513ab335002cd5bc04b28c196e7e971
mrcbq <marcobaquero@gmail.com>
2023-05-24 18:11:24 -0500

    Add .babelrc file for run test with imports, add counter cards test

--- test/counterCards.test.js
+++ test/counterCards.test.js
@@ -12,4 +12,15 @@ describe('getCounterCards', () => {
     // Assert
     expect(result).toBe(3);
   });
+  test('test_container_with_zero_child_elements_returns_correct_count', () => {
+    // Arrange
+    const container = document.createElement('div');
+    container.innerHTML = '';
+
+    // Act
+    const result = getCounterCards(container);
+
+    // Assert
+    expect(result).toBe(0);
+  });
 });

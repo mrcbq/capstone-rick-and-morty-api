7ed6902b8e48a984e42486c45e0c55b1c94badf8
mrcbq <marcobaquero@gmail.com>
2023-05-24 14:51:55 -0500

    Add counter cards to navbar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import getCharacters from './modules/characters.js';
 import createCard from './modules/createCard.js';
 import popupComments from './modules/commentsPopup.js';
 import { postLike, getLikes } from './modules/involvementAPI.js';
+import getRandomArray from './modules/randomNumber.js';
 
 const containerPopup = document.querySelector('.popup');
 const container = document.getElementById('cards-container');
+const counterCards = document.getElementById('counter-cards');
 
-const charactersData = await getCharacters([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+// const charactersData = await getCharacters([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+const charactersData = await getCharacters(getRandomArray(Math.random() * 10 + 1));
+
 
 container.addEventListener('click', async (e) => {
   if (e.target.classList.contains('comments-button')) {
@@ -29,4 +33,5 @@ container.addEventListener('click', async (e) => {
 charactersData.forEach((character) => {
   const card = createCard(character);
   container.appendChild(card);
+  counterCards.textContent = `Characters (${charactersData.length})`;
 });

dfdba809a862b1dc1e86d15941e5fcb4197bda7a
mrcbq <marcobaquero@gmail.com>
2023-05-24 18:31:01 -0500

    End tests, Add coverage folder to .gitignore

--- test/counterCards.test.js
+++ test/counterCards.test.js
@@ -5,10 +5,8 @@ describe('getCounterCards', () => {
     // Arrange
     const container = document.createElement('div');
     container.innerHTML = '<p></p><span></span><div></div>';
-
     // Act
     const result = getCounterCards(container);
-
     // Assert
     expect(result).toBe(3);
   });
@@ -16,11 +14,54 @@ describe('getCounterCards', () => {
     // Arrange
     const container = document.createElement('div');
     container.innerHTML = '';
-
     // Act
     const result = getCounterCards(container);
-
     // Assert
     expect(result).toBe(0);
+  });
+  test('test_container_with_no_child_elements_returns_zero', () => {
+    // Arrange
+    const container = document.createElement('div');
+    // Act
+    const result = getCounterCards(container);
+    // Assert
+    expect(result).toBe(0);
+  });
+  test('test_container_is_null_or_undefined_returns_zero', () => {
+    // Arrange
+    const container = null;
+    // Act
+    const result = getCounterCards(container);
+    // Assert
+    expect(result).toBe(0);
+  });
+  test('test_container_with_large_number_of_child_elements_returns_correct_count', () => {
+    // Arrange
+    const container = document.createElement('div');
+    for (let i = 0; i < 1000; i += 1) {
+      container.appendChild(document.createElement('p'));
+    }
+    // Act
+    const result = getCounterCards(container);
+    // Assert
+    expect(result).toBe(1000);
+  });
+  test('test_container_with_complex_child_element_structure_returns_correct_count', () => {
+    // Arrange
+    const container = document.createElement('div');
+    container.innerHTML = '<div><p></p></div><span><a></a></span>';
+    // Act
+    const result = getCounterCards(container);
+    // Assert
+    expect(result).toBe(2);
+  });
+  test('test_container_has_non_element_child_nodes_returns_correct_count', () => {
+    // Arrange
+    const container = document.createElement('div');
+    container.innerHTML = 'text node<p></p><span></span>';
+    // Act
+    const result = getCounterCards(container);
+    // Assert
+    expect(result).toBe(2);
   });
 });

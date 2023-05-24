fb07c07e1f458cbe4f665f12f5ec9312db548c0f
Luiscarlosvd <lc26cr7@gmail.com>
2023-05-24 18:02:01 -0400

    Fixed test about counterComments

--- test/counterComments.test.js
+++ test/counterComments.test.js
@@ -1,23 +1,21 @@
 import counterComments from "./testCounterComments.js";
 
-
-test('returns the number of comments by taking the parent element as a parameter', () => {
+describe("counterComments", () => {
+  test("returns the number of comments by taking the parent element as a parameter", () => {
     // Arrange
-    const body = document.getElementsByTagName('body');
-    const html = `
-        <ul class="comments-elements">
-            <li><p>02/05/23 Luis Carlos: Hello this is a comment</p></li>
-            <li><p>02/05/23 Marco: Hello this is a comment</p></li>
-            <li><p>02/05/23 Ana: Hello this is a comment</p></li>
-            <li><p>02/05/23 Juan: Hello this is a comment</p></li>
-        </ul>
-    `;
-    body.appendChild(html);
-    const containerComments = document.querySelector('.comments-elements');
+    const containerComments = document.createElement("ul");
+    containerComments.innerHTML = `
+              <li class="individual-comment"><p>02/05/23 Luis Carlos: Hello! This is a comment</p></li>
+              <li class="individual-comment"><p>02/05/23 Marco: Hello! This is a comment</p></li>
+              <li class="individual-comment"><p>02/05/23 Ana: Hello! This is a comment</p></li>
+              <li class="individual-comment"><p>02/05/23 David: Hello! This is a comment</p></li>
+          `;
+    document.body.appendChild(containerComments);
 
     // Act
-    const result = counterComments(containerComments)
+    const result = counterComments(containerComments);
 
     // Assert
     expect(result).toBe(4);
+  });
 });
